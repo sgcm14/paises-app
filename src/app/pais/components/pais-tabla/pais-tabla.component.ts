cd3bd3553ee4334396c216c78c704d2129437c66
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2022-03-23 20:31:51 -0500

    Add files via upload

---
+++ src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Country } from 'src/app/pais/interfaces/pais.interface';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+  styleUrls: []
+})
+export class PaisTablaComponent implements OnInit {
+
+  @Input () paises : Country [] = [];
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
